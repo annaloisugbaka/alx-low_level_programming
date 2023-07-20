ccd8d5d08b35b52fe9f8391fc074d409f367e0ba
annaloisugbaka <annaloisugbaka7@gmail.com>
2023-07-19 17:42:26 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Print _putchar to stdout
+ * Return: 0 Always Success
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
